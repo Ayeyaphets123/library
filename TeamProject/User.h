1319eda0c64f5dd45c6e30c0fd90d44da2bfb2cc
lequanh13 <131572311+lequanh13@users.noreply.github.com>
2023-04-23 12:14:00 +0700

    Update User.h

--- TeamProject/User.h
+++ TeamProject/User.h
@@ -1,27 +1,21 @@
 #include "Book.h"
 #ifndef USER_H
 #define USER_H
+enum accessibilityType {HIGH, MEDIUM, LOW};
 class User
 {
     public:
-        enum accessibilityType
-        {
-            High = 1,
-            Medium,
-            Low
-        };
-        User() : accessibilityLevel(Low) {}
+        User() : accessibilityLevel(LOW) {}
         User(accessibilityType AL);
         virtual ~User() {}
-        void readFreeBook(const Book &b) const;
-        void searchBookTittle(std::string) const;
-        void searchBookSerialNum(std::string) const;
-        void setAccessibilityLevel(accessibilityType);
+        //void readFreeBook(const Book &b) const; //con can ha`m nay khong nhi?
+        void searchByTittle(std::string t) const;
+        void searchBySerial(std::string s) const;
+        void setAccessibilityLevel(accessibilityType t);
         accessibilityType getAccessibilityLevel() const;
         virtual void readBook(const Book &b) = 0;
     private:
         accessibilityType accessibilityLevel;
-
 };
 
 #endif
